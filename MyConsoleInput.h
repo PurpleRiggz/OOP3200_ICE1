aa70b84ec496a2142f5dcc58efa0b81ada4a5352
DPNurple <dpnurple@gmail.com>
2021-09-17 17:37:52 -0400

    Grabbed the completed github code instead of the code from dc connect lol

--- MyConsoleInput.h
+++ MyConsoleInput.h
@@ -1,14 +1,15 @@
 #pragma once
 
 #ifndef MY_CONSOLE_INPUT_H
+
 #define MY_CONSOLE_INPUT_H
 
 #include <limits> 
 #include <cfloat>  // for limits of a double DBL_MIN and DBL_MAX
+#include "MyConsoleInput.cpp"
 
-class ConsoleInput
+namespace ConsoleInput
 {
-public:
 	/**	ReadDouble function
 	 * Reads a valid double value from the console with range checking
 	 *
@@ -16,7 +17,7 @@ public:
 	 * @param  MIN the minimum value the user may enter; defaults to the maximum double.
 	 * @return A validated double input by the user.
 	*/
-	static double ReadDouble(const double MIN = -DBL_MAX, const double MAX = DBL_MAX);
+	double ReadDouble(const double MIN = -DBL_MAX, const double MAX = DBL_MAX);
 
 	/**	ReadInteger function
 	 * Reads a valid int value from the console with range checking
@@ -25,7 +26,7 @@ public:
 	 * @param  MIN the minimum value the user may enter; defaults to the maximum int.
 	 * @return A validated int input by the user.
 	*/
-	static int ReadInteger(const int MIN = INT_MIN, const int MAX = INT_MAX);
+	int ReadInteger(const int MIN = INT_MIN, const int MAX = INT_MAX);
 
-};
+}
 #endif // end of ifndef MY_CONSOLE_INPUT_H
--- main.cpp
+++ main.cpp
@@ -28,42 +28,26 @@ public:
 	*	- called automatically when an obj is instantiated
 	*	- should set values for each member variable
 	*/
-	CartesianPoint(int x = 1, int y = 1);
 
-	// --------------------------------------------------------------------------------
+
 	/* Accessors: Used to query the state of the object
 	*	- never modifies the object
 	*	- should specify const at the end of the prototype/header
 	*/
 
 	// get x
-	int GetX();
+	// get y 
+	// get the distance between this point and a second point
+	// convert the obj to a string	
 
-	// get y
-	int GetY();
-
-	// -------------------------------------------------------------------------------
 	/* Mutator(s): Used to change the state of the object
 	*	- should contain logic to ensure object remains in a valid state.
 	*	- typically sets a member variable to a parameter
 	*/
 
 	// set x
-	void SetX(int x);
-
-	// set y,
-	void SetY(int y);
-
-	// set point (both x and y)
-	void SetPoint(int x, int y);
-
-
-	// get the distance between this point and a second point
-	double GetDistanceTo(CartesianPoint pointTo) const;
-
-	// convert the obj to a string
-	string ToString() const;
-
+	// set y, 
+	// set point (both x and y)	
 
 private:
 	// private data members for the dimensions of the point
@@ -77,16 +61,10 @@ int main()
 {
 	// Declarations
 	// a point called 'origin', initialized to 0, 0
-	CartesianPoint origin = CartesianPoint(0, 0);
-
 	// a point called 'destination'
-	CartesianPoint destination;
-
 	int inputX; // temporary input variable for x
 	int inputY; // temporary input variable for y
-
 	// to store the distance between two points
-	double distance;
 
 	try
 	{
@@ -96,25 +74,23 @@ int main()
 		cout << "\nEnter coordinates of the destination point: " << endl;
 		// prompt for, read, and store x coordinate:
 		cout << "X: ";
-		inputX = ConsoleInput::ReadInteger();
-		destination.SetX(inputX);
+
+
 
 		// prompt for, read, and store y coordinate:
 		cout << "Y: ";
-		inputY = ConsoleInput::ReadInteger();
-		destination.SetY(inputY);
+
 
 
 		// Processing
 		// determine the distance between the two points
-		distance = origin.GetDistanceTo(destination);
+
 
 		// Output 
 		// Show the points and the distance
 		cout << fixed << setprecision(3); // formatting
-		cout << "\nThe Distance between " << origin.ToString()
-			<< " and " << destination.ToString() << " is "
-			<< distance << ". " << endl;
+
+
 	}
 
 	catch (exception& ex)
@@ -129,54 +105,17 @@ int main()
 
 // Class definition section
 
-// constructor for CartesianPoint
-CartesianPoint::CartesianPoint(int x, int y)
-{
-	SetPoint(x, y);
-}
-
-void CartesianPoint::SetPoint(int x, int y)
-{
-	SetX(x);
-	SetY(y);
-}
-
-void CartesianPoint::SetX(int x)
-{
-	myX = x;
-}
-
-void CartesianPoint::SetY(int y)
-{
-	myY = y;
-}
-
-int CartesianPoint::GetX()
-{
-	return myX;
-}
-
-int CartesianPoint::GetY()
-{
-	return myY;
-}
-
-
 /** GetDistanceTo Method for CartesianPoint class
 *	Determines the distance between this point and a second point.
 *	@param	pointTo: CartesianPoint
 *	@return	the distance as a double
 */
-double CartesianPoint::GetDistanceTo(CartesianPoint pointTo) const
+
 {
 	// difference between x values
-	int xDelta = pointTo.myX - myX;
-
 	// difference between y values
-	int yDelta = pointTo.myY - myY;
-
 	// return the formula (based on Pythagorean theorem)
-	return sqrt((xDelta * xDelta) + (yDelta * yDelta));
+
 }
 
 /** ToString Method for CartesianPoint class
@@ -184,14 +123,8 @@ double CartesianPoint::GetDistanceTo(CartesianPoint pointTo) const
 *	@return	the obj state as a string
 */
 
-string CartesianPoint::ToString() const
 {
 	// declare a stringstream object
-	stringstream strOut;
-
 	// build the string
-	strOut << "(" << myX << ", " << myY << ")";
-
 	// return the string
-	return strOut.str();
 }
